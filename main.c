c5cc0b3087eb4c1bf30557974577c7271df09cac
MMXZ <nujuxz@gmail.com>
2015-03-17 15:58:25 -0400

    Added functions to fork/execute commands and wait appropriately. Please take a look at functions forkAndKnife(char argv[][]) and waitingRoom(). Please adjust these to fit with the command parsing stuff.

--- main.c
+++ main.c
@@ -39,6 +39,8 @@ int main()
     printf("Enter an empty line to exit.\n");
     do{
 <<<<<<< HEAD
+
+        	printf("\n$>");
 		//create a list of arguments
 		command = CListCreate();
 		int index = 0, complete = 0, commandIndex = 0;
@@ -131,7 +133,7 @@ int main()
 		}
 
     		fputs(to_parse,stdout);
-        	printf("\n$>");
+		//forkAndKnife(--INSERT COMMAND HERE--);
 		
 		CListDestroy(command);
 	
@@ -142,6 +144,46 @@ int main()
 }
 
 
+/*This function forks a child process and has it execute a command with arguments, passed in function call.
+ * On successful fork, the parent waits for child process in function waitingRoom(int).
+ * If the fork fails, the process exits with status 1.
+ * If the execvp fails, the child process exits with status 1.
+ */
+void forkAndKnife(char argv[][]){ //Assuming command is in a string array. Adjust this as necessary.
+	int pid;
+	switch(pid = fork()){
+	//Child process case
+	case 0:
+		//call execvp here because it searches path for command. We won't have to search it ourselves
+		execvp(argv[0], argv);	//Adjust Here as well.
+		perror("failed execvp");
+		exit(1);
+		break;
+	//fork() fails case
+	case -1:
+		perror("failed fork");
+		exit(1);
+		break;
+	//Parent process case
+	default:
+		ret = waitingRoom(pid);
+		break;
+	}
+	return;
+}
+
+/*Parent process waits in here for child process.
+ *Parent captures and prints s
+ */
+void waitingRoom(){
+
+	int status,pid;
+	//Parent loops on wait until wait returns -1, which is when all child processes have finished.
+	whlie ((pid = wait(&status)) != -1)
+		//Parent prints the exit status of each completed child process.
+		fprintf(stderr, "Child process %d exits with %d.\n",pid, WEXITSTATUS(status));
+	return;
+}
 
 /*Creates a new linked list.*/
 CListPtr CListCreate(){
