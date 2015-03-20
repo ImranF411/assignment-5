f3c750f2b1e56bd1c6bf5168ad51f3c85a4612c6
MMXZ <nujuxz@gmail.com>
2015-03-20 19:22:33 -0400

    IT LIVES!!!!!!!!!!!11 Testing to be done at a later date.

--- main.c
+++ main.c
@@ -6,12 +6,14 @@
 #include <unistd.h>
 
 
-/*Authors: Michael Sabbgh, Imran Farooqui*/
+/*Authors: Michael Sabbagh, Imran Farooqui*/
 
 typedef struct cnode{
 	char ncommand[50] [50];
 	int ccount;
+	int fd[2];
 	struct cnode *next;
+	struct cnode *prev;
 }cnode;
 
 struct clist{
@@ -50,9 +52,12 @@ int searchFuncTable(char *command[], int index);
 
 int fillCommandList(char* to_parse);
 
-char acommand [50][50];
+void forkAndKnife2(cnode* cmdNode);
+
+//char acommand [50][50];
 
 char clcommand [50][50];
+
 fncEntry funcTable[2];
 
 CListPtr commandList;
@@ -78,14 +83,14 @@ int main()
             //Create pipe
             int fd[2];
             pipe(fd);
-	    
+
 	    //Check if empty input
 	    if(to_parse[0] == '\n'){
 		printf("\n$>");
 		CListDestroy(commandList);
 		continue;
 	    }
-	
+
 	    //Count the number of " and '
 	    while(index < strlen(to_parse)){
 		if(to_parse[index] == '\"'){
@@ -96,15 +101,15 @@ int main()
 		}
 		index++;
 	    }
-	   
+
 	    //Check if quotes are mismatched
-     	    if((quoteCheck % 2) || (quoteCheck2 % 2)){
-		printf("Error: mismatched quote\n$>");
-		CListDestroy(commandList);
-		continue;
+        if((quoteCheck % 2) || (quoteCheck2 % 2)){
+            printf("Error: mismatched quote\n$>");
+            CListDestroy(commandList);
+            continue;
 	    }
 	    index = 0;
-	   
+
    	    //Check if input is only spaces and/or tabs
 	    while(to_parse[index] == ' ' || to_parse[index] == '\t' || to_parse[index] == '\n'){
 		index++;
@@ -116,11 +121,11 @@ int main()
             }
 
 	    index = 0;
-	
-	
+
+
 	    fillCommandList(to_parse);
             //Go through to_parse until we have processed all commands
-            while(index < strlen(to_parse) && !complete){
+            /*while(index < strlen(to_parse) && !complete){
 
                 //If we hit a " then get the full argument
                 if(to_parse[index] == '\"'){
@@ -186,10 +191,10 @@ int main()
                     commandIndex = 0;
                     memcpy(myCommand, "\0", 100);
 
-                            forkAndKnife(acommand, aindex, fd, isFirst, 0); //Execute command
+                    //forkAndKnife(acommand, aindex, fd, isFirst, 0); //Execute command
                     isFirst = 0;
 
-                    /*Empty out string holding command*/
+                    /*Empty out string holding command
                     int i = 0;
                     while(i < aindex){
                         memcpy(acommand[i], "\0", 50);
@@ -204,7 +209,6 @@ int main()
                 commandIndex++;
                 index++;
 
-
                 //If we reach the end of the input command / list of commands
                 if(index >= strlen(to_parse)){
                     if(strlen(myCommand) != 0){
@@ -215,24 +219,34 @@ int main()
                         commandIndex = 0;
                     }
                     memcpy(acommand[aindex], "\0", 50);
-                    forkAndKnife(acommand, aindex, fd, isFirst, 1);
+                    //forkAndKnife(acommand, aindex, fd, isFirst, 1);
 
-                    /*empty out string holding command*/
+                    /*empty out string holding command
                     int i = 0;
                     while(i < aindex){
                         memcpy(acommand[i], "\0", 50);
                         i++;
-                    }
+                    }*/
 		    //CPrintList(commandList);
+		    cnode* exec = commandList->head;        //Start at command list's head
+		    for(;exec != NULL; exec = exec->next){  //Loop through all of them
+                pipe(exec->fd);                     //Set up pipe
+                forkAndKnife2(exec);                //Execute command
+                close(exec->fd[1]);                 //Close current write-to pipe, current process is done writing to here.
+                if(exec->prev != NULL)              //Close previous read-from pipe, current process is done reading from here.
+                    close(exec->prev->fd[0]);
+                if(exec->next == NULL)              //Close reading pipe of last command.
+                    close(exec->fd[0]);
+		    }
+		    waitingRoom();
 		    CListDestroy(commandList);
                     complete = 1;
-                }
-            }
+//                }
+//            }
 
     		//forkAndKnife(acommand);
     		//fputs(to_parse,stdout);
-            close(fd[0]); close(fd[1]);	//close pipes.
-    		waitingRoom();			//Wait for child processes to die
+    		//waitingRoom();			//Wait for child processes to die
         	printf("\n$>");
 	}
 
@@ -296,6 +310,49 @@ void forkAndKnife(char acommand[50][50], int index, int pfd[],int isFirst, int i
 	return;
 }
 
+void forkAndKnife2(cnode* cmdNode){ /*electric boogaloo*/
+    int pid;
+	int i = 0;
+	int index = cmdNode->ccount;
+
+	char* pcommand [50];
+
+	while(i < index){
+		pcommand[i] = cmdNode->ncommand[i];
+		//acommand[i];
+		i++;
+	}
+
+	pcommand[index] = NULL;
+
+	if(searchFuncTable(pcommand, index) == 1)
+        return;
+
+	switch(pid = fork()){
+	//Child process case
+	case 0:
+		//call execvp here because it searches path for command. We won't have to search it ourselves
+		//Pipe work done here
+		if(cmdNode->prev != NULL)//Is there a previous command?
+            dup2(cmdNode->prev->fd[0],0); //If so, use its fd[0] for standard input.
+        if(cmdNode->next != NULL)//Is there a next command?
+            dup2(cmdNode->fd[1],1); //If so, use this node's fd[1] for standard output.
+		execvp(pcommand[0], pcommand); //Execute command
+		perror("execvp"); //Houston, we have a problem.
+		exit(1);
+		break;
+	//fork() fails case
+	case -1:
+		perror("fork"); //In the words of President Madagascar "SHUT DOWN EVERYTHING"
+		exit(1);
+		break;
+	//Parent process case
+	default:
+		break;          //Talk about lazy parenting...
+	}
+	return;
+}
+
 /*Parent process waits in here for child process.
  *Parent captures and prints s
  */
@@ -308,11 +365,19 @@ void waitingRoom(){
 		fprintf(stderr, "Child process %d exits with %d.\n",pid, WEXITSTATUS(status));
 	return;
 }
-
+/*Built in function for changing directory "cd"*/
 int cdFunc(int argc, char *argv[]){
-    char* dir = getenv("HOME"); //Default to home environment.
-    if(argc >= 2)
+    char *dir;
+    if(argc = 1){
+            fprintf(stderr,"No directory specified. Going to home directory.\n");
+        return chdir(getenv("HOME")); //Default to home environment.
+    }
+    if(argc = 2)
         dir = argv[1];
+    if(argc > 2){
+        fprintf(stderr,"Too many arguments. Staying in current directory.\n");
+        return 1;
+    }
     int ret = chdir(dir);
     if(ret == 0){
         fprintf(stderr,"%s",getcwd(NULL,0));
@@ -322,7 +387,7 @@ int cdFunc(int argc, char *argv[]){
     }
     return 0;
 }
-
+/*built in function for exiting shell "exit"*/
 int exitFunc(int argc, char *argv[]){
     long code = 0;
     if(argc != 1){
@@ -333,15 +398,12 @@ int exitFunc(int argc, char *argv[]){
     return 0;
 }
 
+/*searches function table for a built in function*/
 int searchFuncTable(char *command[], int index){
     int i = 0;
-    fprintf(stderr,"Built in function: %s\n",command[0]);
+    //fprintf(stderr,"Built in function: %s\n",command[0]);
     for(; i<2;i++){     //Search function table
         if(strcmp(command[0],funcTable[i].name) == 0){  //If function found...
-            int a = 0;
-            for(;a < index;a++){
-                fprintf(stderr,"%s\n",command[a]);
-            }
             int fnc = (*funcTable[i].fnc)(index,command);
             return 1;
         }
@@ -453,7 +515,7 @@ int fillCommandList(char* to_parse){
                     memcpy(clcommand[aindex], "\0", 50);
                     //forkAndKnife(clcommand, aindex, fd, isFirst, 1);
 		   CListInsert(commandList, clcommand, aindex);
-      		   char clcommand [50][50]; 
+      		   char clcommand [50][50];
 		    commandList->count++;
              /*empty out string holding command*/
                     int i = 0;
@@ -494,7 +556,7 @@ int CListInsert(CListPtr list, char command [50] [50], int index){
 		printf("List or data are null in %s, line %d", __FILE__, __LINE__);
 		return 0;
 	}
-	cnode* newNode = (cnode*)malloc(sizeof(cnode));	
+	cnode* newNode = (cnode*)malloc(sizeof(cnode));
 	int i = 0;
 	while(i < index){
 		strcpy(newNode->ncommand[i], command[i]);
@@ -505,12 +567,15 @@ int CListInsert(CListPtr list, char command [50] [50], int index){
 
 	if(list->head == NULL){
 		list->head = newNode;
+		list->head->next = NULL;
+		list->head->prev = NULL;
 	}else{
 		cnode * tmp = list->head;
 		while(tmp->next != NULL){
 			tmp = tmp->next;
 		}
 		tmp->next = newNode;
+		newNode->prev = tmp;
 	}
 
 	return 0;
@@ -524,6 +589,7 @@ int CListRemove(CListPtr list){
 		return 0;
 	}
 	if(list->head != NULL){
+        list->head->next->prev = NULL;
 		list->head = list->head->next;
 		return 1;
 	}
